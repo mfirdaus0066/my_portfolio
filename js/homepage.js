0734a9f6552014e5e2f3416d64dbef5555d4a890
firdaus <mfirdaus120606@gmail.com>
2025-07-12 15:25:21 +0800

    filetheme

--- js/homepage.js
+++ js/homepage.js
@@ -1,16 +1,24 @@
 let lightmode = localStorage.getItem('lightmode')//to store the preferred theme in the local storage (like in your browser)
 const themeBtn = document.getElementById('themeBtn')
+const fileIcon = document.getElementsByClassName('fileIcon')
 
 function enablelightmode()
 {
     document.body.classList.add('lightmode')
     localStorage.setItem('lightmode','active')
+
+    fileIcon[0].style.display = "none"
+    fileIcon[1].style.display = "block"
+    //just like the theme icon case but since the file icon is an image and not a colour i decide to do it this way
 }
 
 function disablelightmode()
 {
     document.body.classList.remove('lightmode')
     localStorage.setItem('lightmode',null)
+
+    fileIcon[1].style.display = "none"
+    fileIcon[0].style.display = "block"
 }
 
 if(lightmode === "active")//to check if the light theme is active so it can be applied when you reload/comeback to the page
