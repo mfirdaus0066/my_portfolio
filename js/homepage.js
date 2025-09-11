5916b12a3c3b9c39fa90f1fdc3fd4935cee40989
firdaus <mfirdaus120606@gmail.com>
2025-09-11 20:25:29 +0800

    window dragging

--- js/homepage.js
+++ js/homepage.js
@@ -42,9 +42,49 @@ function themechange()
 function openFile()
 {
     windowBox.style.visibility = "visible"
+    windowBox.style.left = "50%";
+    windowBox.style.top = "50%";
 }
 
 function closeWindow()
 {
     windowBox.style.visibility = "hidden"
 }
+
+let isDragging = false
+let newX, newY, startX, startY;
+
+windowBox.addEventListener('mousedown', mouseDown)//to grab the window to drag
+
+function mouseDown(e)
+{
+    isDragging = true;
+    startX = e.clientX;
+    startY = e.clientY;
+
+    windowBox.addEventListener('mousemove', mouseMove);//to drag the window when the mouse is on the window
+    windowBox.addEventListener('mouseup', mouseUp);//to stop the dragging when the mouse is not on the window
+}
+
+function mouseMove(e)
+{
+    if(isDragging)
+    {
+        newX = startX - e.clientX;//to get the placement of the mouse
+        newY = startY - e.clientY;
+
+        startX = e.clientX;//to update the placement of the mouse like where it currently is
+        startY = e.clientY;
+
+        windowBox.style.left = (windowBox.offsetLeft - newX) + "px";//to move the window itself
+        windowBox.style.top = (windowBox.offsetTop - newY) + "px";
+    }
+}
+
+function mouseUp(e)
+{
+    isDragging = false;
+    if(!isDragging)
+    document.removeEventListener('mousemove',mouseMove);
+}
+
