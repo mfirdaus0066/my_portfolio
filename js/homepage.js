49df358bc29958b4df1d0aa3d07f4259063e96e3
firdaus <mfirdaus120606@gmail.com>
2025-10-05 22:10:25 +0800

    minimize

--- js/homepage.js
+++ js/homepage.js
@@ -25,8 +25,11 @@ function enablelightmode()
     miniBtn[0].style.display = "none"
     miniBtn[1].style.display = "block"
 
-    tabIconDark.style.display = "none"
-    tabIconLight.style.display = "block"
+    for(let i=0; i< tabIconDark.length; i++)
+    {
+        tabIconDark.style.display = "none"
+        tabIconLight.style.display = "block"
+    }
 }
 
 function disablelightmode()
@@ -46,8 +49,12 @@ function disablelightmode()
     miniBtn[1].style.display = "none"
     miniBtn[0].style.display = "block"
 
-    tabIconDark.style.display = "block"
-    tabIconLight.style.display = "none"
+    for(let i=0; i< tabIconDark.length; i++)
+    {
+        tabIconDark.style.display = "block"
+        tabIconLight.style.display = "none"
+    }
+   
 }
 
 if(lightmode === "active")//to check if the light theme is active so it can be applied when you reload/comeback to the page
@@ -74,8 +81,21 @@ function closeWindow()
     windowBox.style.visibility = "hidden"
 }
 
+let windowloc = -96;
+let isMinimize = false; //to ensure the window is minimize or not
+const content = windowBox.querySelector('.window-content');
+
 function miniWindow()
 {
+        windowBox.style.transform = `translate(${windowloc}%, 8%) scale(0.35)`
+        windowBox.style.transition = "transform 0.5s"
+        windowBox.style.position = "absolute"
+        isMinimize = true
+
+        if(content)
+        {
+              content.style.display = "none" //queryselector is to select the id or class w/o using get...
+        }
     
 }
 
@@ -84,15 +104,31 @@ function shrinkWindow()
     if(windowBox.style.transform === ("translate(-50%, -50%) scale(1)"))
     {   
         windowBox.style.transform =  "translate(-50%, -50%) scale(0.6)"
-        windowBox.style.transistion = "trnasform 0.5s"
+        windowBox.style.transistion = "transform 0.5s"
     }
 
     else
     {
         windowBox.style.transform= "translate(-50%, -50%) scale(1)"// the translate -50% is to make it center
-        windowBox.style.transistion = "trnasform 0.5s"
+        windowBox.style.transistion = "transform 0.5s"
     }
 }
+
+windowBox.addEventListener('click', () =>
+{
+    if(isMinimize)
+    {
+        windowBox.style.transform = "translate(-50%, -50%) scale(1)"
+        windowBox.style.transition = "transform 0.5s"
+        isMinimize = false
+        windowBox.style.position = "fixed"
+
+        if(content)
+        {
+            content.style.display = "block"
+        }
+    }
+})
 
 let isDragging = false
 let newX, newY, startX, startY;
@@ -101,6 +137,7 @@ windowBox.addEventListener('mousedown', mouseDown)//to grab the window to drag
 
 function mouseDown(e)
 {
+    if(isMinimize) return; //to prevent dragging when minimized
     isDragging = true;
     startX = e.clientX;
     startY = e.clientY;
